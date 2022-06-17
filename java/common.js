31a143b8aa0875a71dd0bfe87f8a5c35af0fd57d
psh9112 <106298368+psh9112@users.noreply.github.com>
2022-06-17 10:37:52 +0900

    re

--- java/common.js
+++ java/common.js
@@ -1,12 +1,14 @@
-$('main').load('main.html');
+/* $('main').load('main.html');
 
-let idx;
+let idx=0;
 
 $('header .top a').click(function(){
+
     event.preventDefault(); //페이지의 이동을 차단시켜주는 명령
     let pageUrl = $(this).attr('href'); // 각 a태그의 주소를 받아온다.
     $('main').load(pageUrl); //main 페이지를 로드한다.
-
+    
+    console.log(pageUrl)
     $('header .top a').eq(idx).removeClass('active');
     $(this).addClass('active');
     
@@ -14,3 +16,9 @@ $('header .top a').click(function(){
 
 
 });
+*/
+
+
+
+$('header').load('inc.html header .top');
+$('footer').load('inc.html footer .bottom');
