3566ef2a6a87799be2071216b0ca3d7f73eb81ee
psh9112 <106298368+psh9112@users.noreply.github.com>
2022-06-17 18:03:29 +0900

    re

---
+++ java/slide.js
@@ -0,0 +1,14 @@
+
+
+
+$(function () {
+
+    $(".slide").slick({
+        dots: true,
+        slidesToShow: 4, //컨텐츠를 몇개 보여줄껀지
+        slidesToScroll: 1 // 버튼을 눌렀을 때 컨텐츠가 몇개 보이는지
+    });
+
+});
+
+
