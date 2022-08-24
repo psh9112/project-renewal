76b8d4a579cfc000a7d856d2588beb9192a830b0
psh9112 <106298368+psh9112@users.noreply.github.com>
2022-08-25 03:03:10 +0900

    st_0825

---
+++ js/slide.js
@@ -0,0 +1,17 @@
+
+
+
+$(function () {
+
+    $(".slide").slick({
+        prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><img src="./img/Icon/arrow_left.png"></button>',
+        nextArrow: '<button class="slick-next" aria-label="Next" type="button"><img src="./img/Icon/arrow_right.png"></button>',
+        dots: false,
+        slidesToShow: 4, //컨텐츠를 몇개 보여줄껀지
+        slidesToScroll: 2, // 버튼을 눌렀을 때 컨텐츠가 몇개 보이는지
+    });
+
+});
+
+
+// 모바일, 태블릿 버전 슬라이드 조정 방법
